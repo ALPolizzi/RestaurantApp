3460273946fa96f15dc69a1b358b5c2aad7d5fcc
ALPolizzi <apolizzi@hawk.iit.edu>
2020-12-02 17:27:53 -0600

    Added checkout system with wait time calculation

--- RestaurantApp/js/cart.js
+++ RestaurantApp/js/cart.js
@@ -1,12 +1,8 @@
 
 //cart system
 // Dynamically populate the table with shopping list items.
-function ClearAll(){
-    sessionStorage.removeItem("Chicken Sandwhich");
-	sessionStorage.removeItem("Burger");
-	sessionStorage.removeItem("Double CheeseBurger");
-    doShowAll();
-}
+
+
 function doShowAll() {
     
       
@@ -19,8 +15,8 @@ function doShowAll() {
 				list += "<tr><td>" + "Burger" + "</td>\n<td>"
 				+ sessionStorage.getItem("Burger") + "</td></tr>\n"
 			}
-			if(sessionStorage.getItem("Duoble CheeseBurger")!=null){
-				list+= "<tr><td>" + "Double CheeeseBurger" + "</td>\n<td>"
+			if(sessionStorage.getItem("Double CheeseBurger")!=null){
+				list+= "<tr><td>" + "Double CheeseBurger" + "</td>\n<td>"
 				+ sessionStorage.getItem("Double CheeeseBurger") + "</td></tr>\n";
 			}
 
@@ -30,7 +26,7 @@ function doShowAll() {
         }
         //Bind the data to HTML table.
         document.getElementById('list').innerHTML = list;
-		
+		document.getElementById("cartTitle").innerHTML = "Shopping Cart"
 }
 function SaveItem() {
 
@@ -63,5 +59,30 @@ doShowAll();
 }
 
 
-
 //Cart system above by Matt Zand of smashing magazine.com
+function ClearAll(){
+    sessionStorage.removeItem("Chicken Sandwhich");
+	sessionStorage.removeItem("Burger");
+	sessionStorage.removeItem("Double CheeseBurger");
+    doShowAll();
+}
+
+
+function waitTime(){
+	return localStorage.getItem("queue");
+}
+
+function Checkout(){
+	
+	if(localStorage.getItem("Chicken Sandwhich")==null)localStorage.setItem("Chicken Sandwhich",0)
+	if(localStorage.getItem("Burger")==null)localStorage.setItem("Burger",0)
+	if(localStorage.getItem("Double CheeseBurger")==null)localStorage.setItem("Double CheeseBurger",0)
+	var total = parseInt(localStorage.getItem("Chicken Sandwhich"))*5+parseInt(localStorage.getItem("Burger"))*7+parseInt(localStorage.getItem("Double CheeseBurger"))*8;
+
+if(localStorage.getItem("queue")==null){localStorage.setItem("queue", total)}
+		else {localStorage.setItem("queue", parseInt(localStorage.getItem("queue"))+total);}
+		ClearAll()
+	document.getElementById("cartTitle").innerHTML = "Thank you for your order, please pay $"+total+" at the counter\nYour order will be ready in "+waitTime()+ " minutes";
+	
+}
+
